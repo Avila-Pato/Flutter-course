c7c4ecd7a05f4b92dc14b9d29e359f4cc404be3b
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-09-10 21:05:10 -0300

    starting with flutter

---
+++ variables/dart_basics/bin/dart_basics.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  var number = "hla que ajce";
+  int age = 23;
+  bool isMale = true;
+  double pi = 3.14;
+
+  print(pi);
+  print(isMale);
+  print(number);
+  print(age);
+  print("hello world");
+}
