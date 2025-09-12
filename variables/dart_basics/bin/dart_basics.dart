856d6e5ab2d5504b47968a684448c363752299c8
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-09-12 15:12:15 -0300

    functions

--- variables/dart_basics/bin/dart_basics.dart
+++ variables/dart_basics/bin/dart_basics.dart
@@ -1,40 +1,51 @@
 void main(List<String> arguments) {
-  var number = "hla que ajce";
-  num age2 = 23;
-  String name = "Mi nombre es";
-  int age = 23;
-  bool isMale = true;
-  bool isFemale = false;
-  double pi = 3.14;
+  grettings("Pato");
+  grettings("Jhoana");
+}
 
-  //Tipos fijos
-  const name2 = "Mi nombre es isaias";
+void grettings(String name) {
+  print("Hello $name");
+}
 
-  String fulltexto = "$name y tengo $age";
-  print(fulltexto);
-  String fulltexto2 = "$name2 y tengo $age2";
-  print(fulltexto2);
+void typesAndConversions() {
+  //  var number = "hla que ajce";
+  // num age2 = 23;
+  // String name = "Mi nombre es";
+  // int age = 23;
+  // bool isMale = true;
+  // bool isFemale = false;
+  // double pi = 3.14;
 
-  // Conversiones
-  var number2 = 20.0;
-  var number3 = number2.toInt();
-  var number4 = number2.toString();
+  // //Tipos fijos
+  // const name2 = "Mi nombre es isaias";
 
-  var numero = "43";
-  var numero2 = int.parse(numero);
-  String toDouble = "32.43";
-  double doubleOk = double.parse(toDouble);
+  // String fulltexto = "$name y tengo $age";
+  // print(fulltexto);
+  // String fulltexto2 = "$name2 y tengo $age2";
+  // print(fulltexto2);
 
+  // // Conversiones
+  // var number2 = 20.0;
+  // var number3 = number2.toInt();
+  // var number4 = number2.toString();
+
+  // var numero = "43";
+  // var numero2 = int.parse(numero);
+  // String toDouble = "32.43";
+  // double doubleOk = double.parse(toDouble);
+}
+
+void operations() {
   //Operaciones
-  int a = 2;
-  int b = 3;
+  // int a = 2;
+  // int b = 3;
 
-  int suma = a + b;
-  int resta = a - b;
-  int multi = a * b;
-  double division = a / b;
-  // double divisionSinDecimalv = a ~/ b;
-  int modulo = a % b;
+  // int suma = a + b;
+  // int resta = a - b;
+  // int multi = a * b;
+  // double division = a / b;
+  // // double divisionSinDecimalv = a ~/ b;
+  // int modulo = a % b;
 
   // a += b;
   // a -= b;
@@ -42,20 +53,15 @@ void main(List<String> arguments) {
 
   // a++
   // a--
+}
 
-  print(doubleOk);
-  print(numero2);
-  print(number2);
-  print(number3);
-  print(number4);
+void inputFunction(int a, int b) {
+  int result = a + b;
+  print("El resultado es  $result");
+}
 
-  print(name2);
-  print(age2);
-  print(name);
-  print(pi);
-  print(isFemale);
-  print(isMale);
-  print(number);
-  print(age);
-  print("hello world");
+int outputFunction() {
+  int a = 4;
+  int b = 2;
+  return a + b;
 }
