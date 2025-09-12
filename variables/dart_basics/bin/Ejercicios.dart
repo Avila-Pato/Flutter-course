96144f2187bbdeeab25f586762eff826b9a5fd53
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-09-11 22:42:59 -0300

    add Ejerc

--- variables/dart_basics/bin/Ejercicios.dart
+++ variables/dart_basics/bin/Ejercicios.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   // CALCULADORA BASICA
 
@@ -9,11 +11,11 @@ void main() {
   // 3. calcular la edad restando el ano de nacimiento al ano actual (2025)
   // 4. Mostar elr esultado en un mensaje como "Tienes X anos.."
 
-  String date = "1998";
-  const int currentYear = 2025;
-  int formDate = int.parse(date);
-  int result = currentYear - formDate;
-  print("Tengo $result anos");
+  // String date = "1998";
+  // const int currentYear = 2025;
+  // int formDate = int.parse(date);
+  // int result = currentYear - formDate;
+  // print("Tengo $result anos");
 
   //Ejercicio 2
 
@@ -28,22 +30,62 @@ void main() {
   // 5 - Calcular cuanto debe pagar cada persona
   // 6 Mostrar el resultado en pantalla
 
-  double totalPrice = 19.283;
-  double tip = 2.3;
-  int people = 4;
+  // double totalPrice = 19.283;
+  // double tip = 2.3;
+  // int people = 4;
 
-  double priceWithTip = (totalPrice * (tip / 100)) + totalPrice;
-  String priceWIthTip = (priceWithTip / people).toStringAsFixed(2);
-  print(priceWIthTip);
+  // double priceWithTip = (totalPrice * (tip / 100)) + totalPrice;
+  // String priceWIthTip = (priceWithTip / people).toStringAsFixed(2);
+  // print(priceWIthTip);
 
-  int currentAge = 18;
+  // int currentAge = 18;
 
-  if (currentAge > 18) {
-    print("Eres mayor de edad");
-  } else if (currentAge < 18) {
-    print("Eres menor de edad");
+  // if (currentAge >= 18) {
+  //   print("Eres mayor de edad");
+  // } else if (currentAge < 18) {
+  //   print("Eres menor de edad");
+  // } else {
+  //   print("caracter invalido");
+  // }
+
+  // int userAge = 12;
+
+  // String ageResult = (userAge >= 18)
+  //     ? "Eres amyor de edad"
+  //     : "eres menor dedad";
+  // print(ageResult);
+
+  // Ejercicio 3
+
+  // Escribe un programa que determine si un numero ingresado por el usaurio es positiovo negativo o 0
+
+  //   print("Ingrese un numero");
+  //   int numberType = int.parse(stdin.readLineSync()!);
+
+  //   if (numberType == 0) {
+  //     print("el valor esta en 0  ($numberType)");
+  //   } else if (numberType > 0) {
+  //     print("el valor es mayor a 0 ($numberType)");
+  //   } else if (numberType < 0) {
+  //     print("El valor es negativo ($numberType)");
+  //   }
+
+  // ejercicio 4
+
+  // Escribe un programa quer eciba un numero entre 1 y 12 e imprima el nombre del mes correspondiente del ano
+
+  print("Imprima el mes por numero: ");
+
+  int mothNumber = int.parse(stdin.readLineSync()!);
+
+  switch (mothNumber) {
+    case 1:
+      print("janaury");
+      break;
   }
 }
+
+
 // double bill = double.parse(total);
   // print("El total de la cuenta es $bill");
   // double totalCount = (bill + tip);
--- variables/dart_basics/bin/Entradas.dart
+++ variables/dart_basics/bin/Entradas.dart
@@ -1,10 +1,37 @@
 import 'dart:io';
 
 void main() {
-  int currentYear = 2025;
+  // print("Ingrese una fecha:  ");
+  // String date = stdin.readLineSync()!;
+  // int formatedYear = int.parse(date);
+  // print("La fecha ingresada es $formatedYear");
 
-  print("Ingrese una fecha:  ");
-  String date = stdin.readLineSync()!;
-  int formatedYear = int.parse(date);
-  print("La fecha ingresada es $formatedYear");
+  // print("ingresa un nuumero de la semana: ");
+  // int day = int.parse(stdin.readLineSync()!);
+
+  // switch (day) {
+  //   case 1:
+  //     print("Lunes");
+  //     break;
+  //   case 2:
+  //     print("Martes");
+  //     break;
+  //   case 3:
+  //     print("Miercoles");
+  //     break;
+  //   case 4:
+  //     print("Jueves");
+  //     break;
+  //   case 5:
+  //     print("Viernes");
+  //     break;
+  //   case 6:
+  //     print("Sabado");
+  //     break;
+  //   case 7:
+  //     print("Domingo");
+  //     break;
+  //   default:
+  //     print("No es un dia de la semana");
+  // }
 }
